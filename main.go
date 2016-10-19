d9e9df13fc70bde098394a6a048d5238f212514c
John Weldon <johnweldon4@gmail.com>
2016-10-18 19:03:39 -0700

    .

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/urfave/cli"
 )
@@ -12,14 +14,38 @@ func Main(c *cli.Context) error {
 	if len(from) < 1 {
 		return fmt.Errorf("no source folders specified")
 	}
+
+	exclude := c.StringSlice("exclude")
+
+	r := NewRepository()
+	defer fmt.Printf("Repository: %s\n", r)
+
 	for _, dir := range from {
 		if stat, err := os.Stat(dir); err != nil {
 			return err
 		} else if !stat.IsDir() {
 			return fmt.Errorf("%q is not a folder", dir)
 		}
-		fmt.Printf("archiving %q ...\n", dir)
+		visitor := func(path string, f os.FileInfo, e error) error {
+			if f.IsDir() {
+				return e
+			}
+			for _, ex := range exclude {
+				if strings.Contains(path, ex) {
+					return e
+				}
+			}
+			if err := r.AddFile(path); err != nil {
+				return err
+			}
+			return e
+		}
+
+		if err := filepath.Walk(dir, visitor); err != nil {
+			return err
+		}
 	}
+
 	return nil
 }
 
@@ -30,6 +56,10 @@ func main() {
 		cli.StringSliceFlag{
 			Name:  "source, s",
 			Usage: "source folder(s) to backup",
+		},
+		cli.StringSliceFlag{
+			Name:  "exclude, x",
+			Usage: "folder(s) to exclude", //TODO:better
 		},
 	}
 	if err := app.Run(os.Args); err != nil {
